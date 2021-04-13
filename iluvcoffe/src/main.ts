da321e26142f4343d1c082181d7325131246cd1a
Esneider <e.velandia2164@uniandes.edu.co>
2021-04-13 12:21:59 -0500

    add filter exception

--- iluvcoffe/src/main.ts
+++ iluvcoffe/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
       enableImplicitConversion: true //this allow remove @Type in attribute
     }
   }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
